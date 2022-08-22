4a39918df58f0f70ff847e651be82ca962d6a8cb
ResetReesi <brookmoore718@gmail.com>
2022-08-22 13:11:17 -0700

    tictactoe board set up

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-
+  const [circle, setCircle] = useState("0")
+  const clickPlay = (index) => {
+    if(squares - 1 === - 1)
+  setSquares(0)
+  }
   return (
     <>
+    {/* Creates a column of 9 squares  */}
+
       <h1>Tic Tac Toe</h1>
-      <Square />
-    </>
-  )
+      <div className= "board">
+
+      {squares.map((value,index) => {
+        return(
+      
+      <Square value={value} index={index}/>
+
+        )})}
+      </div>  
+     
+   </>
+   )
 }
+
 
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Square = (props) => {
+  const squareClick = (props) => {
+    props.clickPlay(props.index)
+  }
   return (
-    <div className="square"></div>
+    <div className="square" onClick={squareClick}>{props.value}</div>
   )
 }
 export default Square
+
